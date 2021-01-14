e5463f137623659443e69b531648e4304b3dfc88
faith176 <feyi.adesanya176@gmail.com>
2021-01-14 15:46:49 -0700

    adjusting colours and themes

--- app/src/main/java/com/example/meditationapp/BreatheActivity.java
+++ app/src/main/java/com/example/meditationapp/BreatheActivity.java
@@ -15,12 +15,21 @@ public class BreatheActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathe);
 
-        TextView circle1 = (TextView) this.findViewById(R.id.circle);
 
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         Path path = new Path();
+
+        TextView circle1 = (TextView) this.findViewById(R.id.circle);
         path.arcTo(0f, 0f, 1000f, 1000f, 0f, 359f, true); //with first four parameters you determine four edge of a rectangle by pixel , and fifth parameter is the path'es start point from circle 360 degree and sixth parameter is end point of path in that circle
         ObjectAnimator animator = ObjectAnimator.ofFloat(circle1 , View.X, View.Y, path); //at first parameter (view) put the target view
         animator.setDuration(2000);
         animator.start();
     }
+
 }
